ad42ce757c43ec36c66e6ef03b2d7d6986b4af5e
Ilaria Franchini <i.franchini@apuliasoftware.it>
2019-06-07 12:44:02 +0200

    [ADD] l10n_it_statutory_financial_statements_mapping

---
+++ l10n_it_statutory_financial_statements_mapping/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Francesco Apruzzese
+# Copyright 2019 Ilaria Franchini
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Statutory financial statements mapping",
+    "version": "10.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/l10n-italy",
+    "author": "Odoo Community Association (OCA)",
+    "maintainers": ["ilaria-franchini","OpenCode"],
+    "license": "LGPL-3",
+    "post_init_hook": "cee_group_mapping_init",
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "l10n_it_statutory_financial_statements",
+        "l10n_it",
+    ],
+}
